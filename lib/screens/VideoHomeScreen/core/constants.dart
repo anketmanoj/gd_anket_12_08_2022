fc06ad90b1fa649426cf9c53ccc36e46e66e567f
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2022-08-19 16:38:43 +0900

    preloading much better

--- lib/screens/VideoHomeScreen/core/constants.dart
+++ lib/screens/VideoHomeScreen/core/constants.dart
@@ -1,9 +1,9 @@
 // Customize
-const int kPreloadLimit = 3;
+const int kPreloadLimit = 2;
 
 // Customize
 const int kNextLimit = 5;
 
 // For better UX, latency should be minimum.
 // For demo: 2s is taken but something under a second will be better
-const int kLatency = 2;
+const int kLatency = 1;
