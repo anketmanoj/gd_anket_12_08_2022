e3924521b67ff94410aa29153a3106790b59a512
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2022-08-29 12:47:40 +0900

    following tab done, next apple pay

--- lib/providers/homeScreenProvider.dart
+++ lib/providers/homeScreenProvider.dart
@@ -1,4 +1,7 @@
+import 'package:diamon_rose_app/constants/Constantcolors.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class HomeScreenProvider with ChangeNotifier {
   bool _isHomeScreen = true;
@@ -7,5 +10,30 @@ class HomeScreenProvider with ChangeNotifier {
   setHomeScreen(bool value) {
     _isHomeScreen = value;
     notifyListeners();
+  }
+
+  ConstantColors constantColors = ConstantColors();
+  Widget topTabBar(
+      BuildContext context, int index, PageController pageController) {
+    return Center(
+      child: ToggleSwitch(
+        initialLabelIndex: index,
+        minWidth: 100.w,
+        totalSwitches: 2,
+        activeBgColor: [
+          constantColors.navButton,
+          constantColors.navButton,
+        ],
+        inactiveBgColor: constantColors.bioBg,
+        labels: ["Recommended", "Following"],
+        changeOnTap: true,
+        onToggle: (val) {
+          pageController.jumpToPage(
+            val!,
+          );
+          notifyListeners();
+        },
+      ),
+    );
   }
 }
--- lib/screens/homePage/homescreen.dart
+++ lib/screens/homePage/homescreen.dart
@@ -1,78 +1,118 @@
 import 'dart:developer';
+import 'dart:io';
 
+import 'package:diamon_rose_app/providers/homeScreenProvider.dart';
 import 'package:diamon_rose_app/providers/recommendedProvider.dart';
+import 'package:diamon_rose_app/screens/VideoHomeScreen/following_video_page.dart';
 import 'package:diamon_rose_app/screens/VideoHomeScreen/recommended_video_page.dart';
 import 'package:diamon_rose_app/screens/homePage/FollowingVideosFeed.dart';
 import 'package:diamon_rose_app/screens/homePage/RecommendedVideosFeed.dart';
 import 'package:diamon_rose_app/widgets/global.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 import 'package:toggle_switch/toggle_switch.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
 
-  ValueNotifier<bool> recommededSelected = ValueNotifier<bool>(true);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController homeScreenTabsController = PageController();
+  ValueNotifier<int> pageIndex = ValueNotifier<int>(0);
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    final RecommendedProvider recommendedProvider =
-        Provider.of<RecommendedProvider>(context, listen: false);
-
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          AnimatedBuilder(
-            animation: Listenable.merge([
-              recommededSelected,
-            ]),
-            builder: (context, _) {
-              switch (recommededSelected.value) {
-                case true:
-                  return RecommendedVideoPage();
-
-                case false:
-                  return FollowingVideosFeed(
-                    recommendedProvider: recommendedProvider,
-                  );
-              }
-
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-          ),
-          Positioned(
-            top: size.height * 0.06,
-            left: 10,
-            right: 10,
-            child: Center(
-              child: ToggleSwitch(
-                minWidth: size.width,
-                totalSwitches: 2,
-                activeBgColor: [
-                  constantColors.navButton,
-                  constantColors.navButton,
-                ],
-                labels: ["Recommended", "Following"],
-                onToggle: (index) {
-                  switch (index) {
-                    case 0:
-                      recommededSelected.value = true;
-                      break;
-                    case 1:
-                      recommededSelected.value = false;
-                      break;
-                  }
+    return ValueListenableBuilder(
+      valueListenable: pageIndex,
+      builder: (context, page, _) {
+        return Scaffold(
+          backgroundColor: Colors.black,
+          body: Stack(
+            children: [
+              PageView(
+                controller: homeScreenTabsController,
+                physics: const NeverScrollableScrollPhysics(),
+                onPageChanged: (page) {
+                  pageIndex.value = page;
                 },
+                children: [
+                  RecommendedVideoPage(),
+                  FollowingVideoPage(),
+                ],
               ),
-            ),
-          )
-        ],
-      ),
+              Positioned(
+                top: 6.h,
+                left: 10,
+                right: 10,
+                child: Provider.of<HomeScreenProvider>(context, listen: false)
+                    .topTabBar(
+                  context,
+                  pageIndex.value,
+                  homeScreenTabsController,
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
+    // return Scaffold(
+    //   backgroundColor: Colors.black,
+
+    // body: Stack(
+    //   children: [
+    //     AnimatedBuilder(
+    //       animation: Listenable.merge([
+    //         recommededSelected,
+    //       ]),
+    //       builder: (context, _) {
+    //         switch (recommededSelected.value) {
+    //           case true:
+    //             return RecommendedVideoPage();
+
+    //           case false:
+    //             return FollowingVideosFeed(
+    //               recommendedProvider: recommendedProvider,
+    //             );
+    //         }
+
+    //         return Center(
+    //           child: CircularProgressIndicator(),
+    //         );
+    //       },
+    //     ),
+    //     Positioned(
+    //       top: size.height * 0.06,
+    //       left: 10,
+    //       right: 10,
+    //       child: Center(
+    //         child: ToggleSwitch(
+    //           minWidth: size.width,
+    //           totalSwitches: 2,
+    //           activeBgColor: [
+    //             constantColors.navButton,
+    //             constantColors.navButton,
+    //           ],
+    //           labels: ["Recommended", "Following"],
+    //           onToggle: (index) {
+    //             switch (index) {
+    //               case 0:
+    //                 recommededSelected.value = true;
+    //                 break;
+    //               case 1:
+    //                 recommededSelected.value = false;
+    //                 break;
+    //             }
+    //           },
+    //         ),
+    //       ),
+    //     )
+    //   ],
+    // ),
+    // );
   }
 }
