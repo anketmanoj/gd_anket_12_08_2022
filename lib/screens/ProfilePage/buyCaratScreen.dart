94b224fd1c77571ea8e29973531b298e81a81b41
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2022-09-09 13:29:53 +0900

    trying in-app purchase with sandbox apple user

--- lib/screens/ProfilePage/buyCaratScreen.dart
+++ lib/screens/ProfilePage/buyCaratScreen.dart
@@ -1,20 +1,22 @@
 import 'package:diamon_rose_app/services/PurchaseCaratsModel.dart';
 import 'package:diamon_rose_app/widgets/global.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
 class BuyCaratScreen extends StatelessWidget {
   BuyCaratScreen({Key? key}) : super(key: key);
 
   final List<PurchaseCarats> carats = [
     PurchaseCarats(price: 1.99, name: "1 Carat"),
-    PurchaseCarats(price: 6.99, name: "5 Carats"),
-    PurchaseCarats(price: 12.99, name: "10 Carats"),
-    PurchaseCarats(price: 38.99, name: "30 Carats"),
-    PurchaseCarats(price: 64.99, name: "50 Carats"),
-    PurchaseCarats(price: 129.99, name: "100 Carats"),
-    PurchaseCarats(price: 249.99, name: "200 Carats"),
-    PurchaseCarats(price: 349.99, name: "300 Carats"),
-    PurchaseCarats(price: 599.99, name: "500 Carats"),
+    PurchaseCarats(price: 7.99, name: "5 Carats"),
+    PurchaseCarats(price: 13.99, name: "10 Carats"),
+    PurchaseCarats(price: 39.99, name: "30 Carats"),
+    PurchaseCarats(price: 74.99, name: "50 Carats"),
+    PurchaseCarats(price: 149.99, name: "100 Carats"),
+    PurchaseCarats(price: 299.99, name: "200 Carats"),
+    PurchaseCarats(price: 399.99, name: "300 Carats"),
+    PurchaseCarats(price: 699.99, name: "500 Carats"),
   ];
 
   @override
@@ -31,6 +33,188 @@ class BuyCaratScreen extends StatelessWidget {
             return Padding(
               padding: const EdgeInsets.only(bottom: 10),
               child: ListTile(
+                onTap: () {
+                  Get.bottomSheet(
+                    Container(
+                      height: 25.h,
+                      width: 100.w,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: constantColors.navButton,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 150),
+                            child: Divider(
+                              thickness: 4,
+                              color: constantColors.whiteColor,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Container(
+                              height: 50,
+                              width: 100.w,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          constantColors.bioBg),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Get.dialog(
+                                    SimpleDialog(
+                                      backgroundColor:
+                                          constantColors.whiteColor,
+                                      title: Text(
+                                        "Pending in-app purchase approval",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: constantColors.black,
+                                        ),
+                                      ),
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Text(
+                                            "We've submitted our in-app purchase approval request for all the Carat tiers shown",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                color: constantColors.black),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Row(
+                                            children: [
+                                              Expanded(
+                                                child: ElevatedButton(
+                                                  style: ButtonStyle(
+                                                    foregroundColor:
+                                                        MaterialStateProperty
+                                                            .all<Color>(
+                                                                Colors.white),
+                                                    backgroundColor:
+                                                        MaterialStateProperty
+                                                            .all<Color>(
+                                                                constantColors
+                                                                    .navButton),
+                                                    shape: MaterialStateProperty
+                                                        .all<
+                                                            RoundedRectangleBorder>(
+                                                      RoundedRectangleBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(20),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  onPressed: Get.back,
+                                                  child: Text(
+                                                    "Understood!",
+                                                    style:
+                                                        TextStyle(fontSize: 12),
+                                                  ),
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                width: 10,
+                                              ),
+                                              Expanded(
+                                                child: ElevatedButton(
+                                                  style: ButtonStyle(
+                                                    foregroundColor:
+                                                        MaterialStateProperty
+                                                            .all<Color>(
+                                                                Colors.white),
+                                                    backgroundColor:
+                                                        MaterialStateProperty
+                                                            .all<Color>(
+                                                                constantColors
+                                                                    .navButton),
+                                                    shape: MaterialStateProperty
+                                                        .all<
+                                                            RoundedRectangleBorder>(
+                                                      RoundedRectangleBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(20),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  onPressed: Get.back,
+                                                  child: Text(
+                                                    "View Items",
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    barrierDismissible: false,
+                                  );
+                                },
+                                child: Text(
+                                  "Apple App Store",
+                                  style: TextStyle(
+                                    color: constantColors.navButton,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Container(
+                              height: 50,
+                              width: 100.w,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          constantColors.bioBg),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  ),
+                                ),
+                                onPressed: () async {},
+                                child: Text(
+                                  "Glamorous Diastation Direct Payment",
+                                  style: TextStyle(
+                                    color: constantColors.navButton,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
                 leading: Image.asset("assets/carats/${index}.png"),
                 title: Text(carats[index].name),
                 trailing: Text("\$${carats[index].price}"),
