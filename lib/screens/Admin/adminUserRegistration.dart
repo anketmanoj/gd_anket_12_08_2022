d703b0c8c1b6c8aa466bbb5a4ba7ffa9292ddb10
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2022-11-12 23:05:48 +0900

    launching version 1.2.98

--- lib/screens/Admin/adminUserRegistration.dart
+++ lib/screens/Admin/adminUserRegistration.dart
@@ -207,43 +207,61 @@ class AdminRegisterUser extends StatelessWidget {
               }
             }
 
+            bool checkExists =
+                await Provider.of<FirebaseOperations>(context, listen: false)
+                    .checkUserExists(useruid: adminCreatedUser!.uid);
+
             // final String? _getToken =
             //     await FirebaseMessaging.instance.getToken();
 
-            await Provider.of<FirebaseOperations>(context, listen: false)
-                .adminCreateUserCollection(
-              context: context,
-              data: {
-                "token": "emptyToken",
-                "useruid": adminCreatedUser!.uid,
-                "username": "${_userConfirmEmailController.text}",
-                "useremail": "${_userConfirmEmailController.text}",
-                "userrealname": "${_userConfirmEmailController.text}",
-                "address": "",
-                "usercontactnumber": "",
-                "usergender": "",
-                "userimage":
-                    "https://firebasestorage.googleapis.com/v0/b/gdfe-ac584.appspot.com/o/bgImagesAR%2FGDapplogo.png?alt=media&token=9a23d52a-2282-4eb7-a751-a8e4fc7b7f8f",
-                // "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-                "userbio": "",
-                "usertiktokurl": "",
-                "userinstagramurl": "",
-                "userfacebookurl": "",
-                "userdob": Timestamp.now(),
-                "usercreatedat": Timestamp.now(),
-                "isverified": false,
-                "usercover":
-                    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ff1e3a0b-d453-4f25-9d40-b639ea34eac6/d8b0e2q-c3445053-675a-4952-9be8-11884fd5c7d7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ZmMWUzYTBiLWQ0NTMtNGYyNS05ZDQwLWI2MzllYTM0ZWFjNlwvZDhiMGUycS1jMzQ0NTA1My02NzVhLTQ5NTItOWJlOC0xMTg4NGZkNWM3ZDcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TNA3OxHyji3j7IYYhoKKMv0Z9RkWkP-pcdTdxLU6h3E",
-                'usersearchindex': indexList,
-                'totalmade': 0,
-                'paypal': '',
-                'percentage': 33,
-              },
-              userUid: adminCreatedUser.uid,
-            );
-            accountCreated.value = true;
+            if (checkExists == false) {
+              await Provider.of<FirebaseOperations>(context, listen: false)
+                  .adminCreateUserCollection(
+                context: context,
+                data: {
+                  "token": "emptyToken",
+                  "useruid": adminCreatedUser.uid,
+                  "username": "${_userConfirmEmailController.text}",
+                  "useremail": "${_userConfirmEmailController.text}",
+                  "userrealname": "${_userConfirmEmailController.text}",
+                  "address": "",
+                  "usercontactnumber": "",
+                  "usergender": "",
+                  "userimage":
+                      "https://firebasestorage.googleapis.com/v0/b/gdfe-ac584.appspot.com/o/bgImagesAR%2FGDapplogo.png?alt=media&token=9a23d52a-2282-4eb7-a751-a8e4fc7b7f8f",
+                  // "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+                  "userbio": "",
+                  "usertiktokurl": "",
+                  "userinstagramurl": "",
+                  "userfacebookurl": "",
+                  "userdob": Timestamp.now(),
+                  "usercreatedat": Timestamp.now(),
+                  "isverified": false,
+                  "usercover":
+                      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ff1e3a0b-d453-4f25-9d40-b639ea34eac6/d8b0e2q-c3445053-675a-4952-9be8-11884fd5c7d7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ZmMWUzYTBiLWQ0NTMtNGYyNS05ZDQwLWI2MzllYTM0ZWFjNlwvZDhiMGUycS1jMzQ0NTA1My02NzVhLTQ5NTItOWJlOC0xMTg4NGZkNWM3ZDcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TNA3OxHyji3j7IYYhoKKMv0Z9RkWkP-pcdTdxLU6h3E",
+                  'usersearchindex': indexList,
+                  'totalmade': 0,
+                  'paypal': '',
+                  'percentage': 33,
+                },
+                userUid: adminCreatedUser.uid,
+              );
+              accountCreated.value = true;
 
-            Get.back();
+              Get.back();
+            } else {
+              Get.back();
+              Get.dialog(
+                SimpleDialog(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text("User already exists!"),
+                    ),
+                  ],
+                ),
+              );
+            }
 
             // ignore: avoid_catches_without_on_clauses
           } catch (e) {
